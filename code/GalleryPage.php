c0f52416f1d7acc15511c5944f3f0351fef82436
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2015-02-08 12:27:07 +1300

    More advanced galleries.

--- code/GalleryPage.php
+++ code/GalleryPage.php
@@ -1,7 +1,22 @@
 <?php
 
 class GalleryPage extends Page {
-
+	static $db = array(
+		'PreviewMax' => 'Int',
+	);
+	static $defaults = array(
+		'PreviewMax' => 12,
+	);
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab('Root.Main', TextField::create('PreviewMax'), 'Content');
+		return $fields;
+	}
+	public function OrderedImagesLimited() {
+		//$limit = $this->PreviewMax;
+		//print_r($this->OrderedImages());
+		return $this->OrderedImages()->limit($this->PreviewMax);
+	}
 }
 
 class GalleryPage_Controller extends Page_Controller {
--- templates/Layout/GalleryPage.ss
+++ templates/Layout/GalleryPage.ss
@@ -1,6 +1,11 @@
-
-<% loop OrderedImages %>
-	<a class="fancybox" data-fancybox-group="gallery" href="$Filename" title="$Caption">
-		$SetSize(250,250)
-	</a>
-<% end_loop %>
+<h1>$Title</h1>
+<article>
+    $Content
+</article>
+<div class="gallery-images">
+    <% loop OrderedImages %>
+        <a class="fancybox" data-fancybox-group="gallery" href="$Filename" title="$Caption">
+            $SetSize(250,250)
+        </a>
+    <% end_loop %>
+</div>
